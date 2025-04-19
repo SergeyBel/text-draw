7f7fa093de2ecf5dc3b924aebe640af882e55b4c
SergeyBel <belovseregas@gmail.com>
2025-04-19 17:13:20 +0300

    ImageRenderStyle

--- src/Render/ImageRender/ImageRender.php
+++ src/Render/ImageRender/ImageRender.php
@@ -10,11 +10,13 @@ use ConsoleDraw\Figure\Pixel\PixelMatrix;
 use ConsoleDraw\Plane\Size;
 use ConsoleDraw\Render\RenderInterface;
 use GdImage;
+use function Symfony\Component\String\s;
 
 class ImageRender implements RenderInterface
 {
     private PixelMatrix $matrix;
     private Size $size;
+    private ImageRenderStyle $style;
 
     public function __construct(
         int $width,
@@ -22,6 +24,8 @@ class ImageRender implements RenderInterface
     ) {
         $this->size = new Size($width, $height);
         $this->matrix = new PixelMatrix();
+
+        $this->style = new ImageRenderStyle();
         $this->clear();
     }
 
@@ -32,17 +36,9 @@ class ImageRender implements RenderInterface
         $imageWidth = $this->size->getWidth() * $charWidth;
         $imageHeight = $this->size->getHeight() * $charHeight;
 
-        $image = imagecreatetruecolor($imageWidth, $imageHeight);
-        $backgroundColor = imagecolorallocate($image, 255, 255, 255);
-        $textColor = imagecolorallocate($image, 0, 0, 0);
-        imagefill($image, 0, 0, $backgroundColor);
-
-        foreach ($this->matrix->getPixels() as $pixel) {
-            $this->drawPixel($image, $pixel, $charWidth, $charHeight, $textColor);
-        }
-
-        imagepng($image, $filename);
-        imagedestroy($image);
+        $image = $this->createImage($imageWidth, $imageHeight);
+        $this->drawPixels($image, $charWidth, $charHeight);
+        $this->saveImage($filename, $image);
     }
 
     public function addFigure(FigureInterface $figure): static
@@ -61,17 +57,63 @@ class ImageRender implements RenderInterface
         $this->matrix->clear();
     }
 
+    public function getStyle(): ImageRenderStyle
+    {
+        return $this->style;
+    }
+
+    public function setStyle(ImageRenderStyle $style): static
+    {
+        $this->style = $style;
+        return $this;
+    }
+
+    private function createImage(int $imageWidth, int $imageHeight): GdImage
+    {
+        $image = imagecreatetruecolor($imageWidth, $imageHeight);
+        $backgroundColor = imagecolorallocate(
+            $image,
+            $this->style->getBackgroundColor()->getRed(),
+            $this->style->getBackgroundColor()->getGreen(),
+            $this->style->getBackgroundColor()->getBlue()
+        );
+
+        imagefill($image, 0, 0, $backgroundColor);
+
+        return $image;
+    }
+
+    private function drawPixels(GdImage $image, int $charWidth, int $charHeight): void
+    {
+        $textColor = imagecolorallocate(
+            $image,
+            $this->style->getTextColor()->getRed(),
+            $this->style->getTextColor()->getGreen(),
+            $this->style->getTextColor()->getBlue()
+        );
+        foreach ($this->matrix->getPixels() as $pixel) {
+            $this->drawPixel($image, $pixel, $charWidth, $charHeight, $textColor);
+        }
+    }
+
+
     private function drawPixel(GdImage $image, Pixel $pixel, int $charWidth, int $charHeight, int $textColor): void
     {
         $x = $pixel->getPoint()->getX() * $charWidth;
         $y = ($pixel->getPoint()->getY() + 1) * $charHeight;
 
-        imagefttext($image, 16, 0, $x, $y, $textColor, './font/UbuntuMono-Regular.ttf', $pixel->getChar());
+        imagefttext($image, $this->style->getFontSize(), 0, $x, $y, $textColor, $this->style->getFont(), $pixel->getChar());
+    }
+
+    private function saveImage(string $filename, GdImage $image): void
+    {
+        imagepng($image, $filename);
+        imagedestroy($image);
     }
 
     private function getCharWidth(): int
     {
-        $box = imageftbbox(16, 0, './font/UbuntuMono-Regular.ttf', 'W');
+        $box = imageftbbox($this->style->getFontSize(), 0, $this->style->getFont(), 'W');
         return abs($box[2] - $box[0]);
     }
 
@@ -82,7 +124,7 @@ class ImageRender implements RenderInterface
          * imageftbbox has a bug on determine char height so use one height for all symbols
          * see https://www.php.net/manual/ru/function.imagettfbbox.php#97211
          */
-        $box = imageftbbox(16, 0, './font/UbuntuMono-Regular.ttf', '|');
+        $box = imageftbbox($this->style->getFontSize(), 0, $this->style->getFont(), '|');
         return abs($box[1] - $box[7]);
     }
 }
---
+++ src/Render/ImageRender/ImageRenderStyle.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace ConsoleDraw\Render\ImageRender;
+
+class ImageRenderStyle
+{
+    private int $fontSize = 16;
+    private string $font = 'font/UbuntuMono-Regular.ttf';
+    private RgbColor $backgroundColor;
+    private RgbColor $textColor;
+
+    public function __construct()
+    {
+        $this->backgroundColor = new RgbColor(255, 255, 255);
+        $this->textColor = new RgbColor(0, 0, 0);
+    }
+
+
+    public function getFontSize(): int
+    {
+        return $this->fontSize;
+    }
+
+    public function setFontSize(int $fontSize): static
+    {
+        $this->fontSize = $fontSize;
+        return $this;
+    }
+
+    public function getFont(): string
+    {
+        return $this->font;
+    }
+
+    public function setFont(string $font): static
+    {
+        $this->font = $font;
+        return $this;
+    }
+
+    public function getBackgroundColor(): RgbColor
+    {
+        return $this->backgroundColor;
+    }
+
+    public function setBackgroundColor(RgbColor $backgroundColor): static
+    {
+        $this->backgroundColor = $backgroundColor;
+        return $this;
+    }
+
+    public function getTextColor(): RgbColor
+    {
+        return $this->textColor;
+    }
+
+    public function setTextColor(RgbColor $textColor): static
+    {
+        $this->textColor = $textColor;
+        return $this;
+    }
+
+
+
+
+
+}
