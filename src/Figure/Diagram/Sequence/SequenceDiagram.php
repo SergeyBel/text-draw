1c24761de7973f95071597ccd36d532cbf32aef3
SergeyBel <belovseregas@gmail.com>
2025-05-12 21:16:44 +0300

    fix

---
+++ src/Figure/Diagram/Sequence/SequenceDiagram.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace TextDraw\Figure\Diagram\Sequence;
+
+use TextDraw\Figure\Base\BaseFigure;
+use TextDraw\Figure\Pixel\PixelMatrix;
+
+class SequenceDiagram extends BaseFigure
+{
+    private array $actors = [];
+    private array $steps = [];
+
+
+    public function draw(): PixelMatrix
+    {
+
+    }
+
+    public function addActors()
+    {
+
+    }
+
+    public function addAction()
+    {
+
+    }
+
+    public function addMessage()
+    {
+
+    }
+
+}
